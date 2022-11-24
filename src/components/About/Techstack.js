c661efe9e2ac1d305e14d43304d0defbd4b4f371
shubham Kumar <shubham.kumar@rupyz.com>
2022-11-24 21:29:32 +0530

    new update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
-  DiPython,
   DiGit,
 } from "react-icons/di";
 import {
-  SiPytorch,
   SiFirebase,
   SiNextdotjs,
 } from "react-icons/si";
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/shubham.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -12,11 +12,11 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 //   "https://github.com/kumarshubham10021998/Portfolio-Shubham/blob/main/src/Assets/shubham.pdf";
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  // const [width, setWidth] = useState(1200);
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+  // useEffect(() => {
+  //   setWidth(window.innerWidth);
+  // }, []);
 
   return (
     <div>
@@ -33,12 +33,12 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button> */}
         </Row>
-
-        <Row className="resume">
+{/* 
+        <Row className="resume"> */}
           {/* <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document> */}
-        </Row>
+        {/* </Row> */}
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
